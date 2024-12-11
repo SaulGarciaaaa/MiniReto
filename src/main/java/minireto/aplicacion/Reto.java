865c71b0334c9cc3b93a86d9f3451d88be4712a6
blackclaw01 <bekeleSanchez@gmail.com>
2024-12-11 18:47:39 +0100

    Update Reto.java

--- src/main/java/minireto/aplicacion/Reto.java
+++ src/main/java/minireto/aplicacion/Reto.java
@@ -18,9 +18,55 @@ public class Reto {
     private ActividadDeportiva tipo;
     private double distancia;
     private double desnivelNegativo;
-    private double desnivelPositivo;
+    private double desnivelPositivo;//desnivel validir con condicional
     private String periodoRealizacion; // No esta mal, se pide un texto
     private String comentario;
     private Waypoint[] puntosPaso;
     private int[] valoraciones;
+    private int contadorWaypoint;    
+
+    public Reto(int nReto, Usuario creador, String nombreReto, LocalDateTime dateTimeCreacion, ActividadDeportiva tipo, double distancia, double desnivelNegativo, double desnivelPositivo, String periodoRealizacion, String comentario, Waypoint[] puntosPaso, int[] valoraciones, int contadorWaypoint) {
+        this.nReto = nReto;
+        this.creador = creador;
+        this.nombreReto = nombreReto;
+        this.dateTimeCreacion = dateTimeCreacion;
+        this.tipo = tipo;
+        this.distancia = distancia;
+        this.periodoRealizacion = periodoRealizacion;
+        this.comentario = comentario;
+        this.puntosPaso = puntosPaso;
+        this.valoraciones = valoraciones;
+        this.contadorWaypoint = contadorWaypoint;
+        contadorWaypoint++;
+        if(desnivelNegativo<=0){
+            this.desnivelNegativo=desnivelNegativo;
+        }else{
+            this.desnivelNegativo=0;
+        }
+        if(desnivelPositivo>=0){
+            this.desnivelPositivo=desnivelPositivo;
+        }else{
+            this.desnivelPositivo=0;
+        }
+    }
+  
+    public void registraInfoWaypoints(){
+        Scanner teclado= new Scanner(System.in);
+        
+        System.out.println(" indica el tipo de waypont o punto de control: ");
+        String tipo= teclado.nextLine();
+        System.out.println(" escribe el nombre de waypont: ");
+        String nombre= teclado.nextLine();
+           System.out.println("indica a que distancia se encuentra del inicio el waypoint: ");
+        double distancia=teclado.nextDouble();
+        System.out.println(" indica el reto: ");
+        String reto= teclado.nextLine();
+           
+        System.out.println(" indica la latitud: ");
+        double latitud= teclado.nextDouble();
+        System.out.println(" indica la longitud: ");
+        double longitud= teclado.nextDouble();
+        Waypoint waypoint= new Waypoint(this.contadorWaypoint,reto,nombre,distancia,tipo,latitud,longitud);
+    }
+
 }

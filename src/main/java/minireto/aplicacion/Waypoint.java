068ab09f20357cfd31e65ed11c7cd5cda9cb885f
blackclaw01 <bekeleSanchez@gmail.com>
2024-12-11 18:48:33 +0100

    Update Waypoint.java

--- src/main/java/minireto/aplicacion/Waypoint.java
+++ src/main/java/minireto/aplicacion/Waypoint.java
@@ -10,10 +10,30 @@ package minireto.aplicacion;
  */
 public class Waypoint {
     private int numeroWaypoint; //Segun el numero de puntos que se han creado anteriormente podria ser 300
-    private int[] posicionReto; //Posicion dentro de los puntos de SU reto
     private String reto;
+    private String nombreReto;
     private double distanciaInicio;
     private String tipo;//Puente, collado, rio....
-    private String coordenadas;// Se pasa longitud y latitud a String 
+    private double latitud;
+    private double longitud;
+
+    public Waypoint(int numeroWaypoint, String reto, String nombre, double distanciaInicio, String tipo, double latitud, double longitud) {
+        this.numeroWaypoint = numeroWaypoint;
+        this.reto = reto;
+        this.nombreReto = nombre;
+        this.distanciaInicio = distanciaInicio;
+        this.tipo = tipo;
+        this.latitud= latitud;
+        this.longitud= longitud;
+    }
+
+    public String getNombreReto() {
+        return nombreReto;
+    }
     
-}
+    @Override
+    public String toString(){
+    
+    return String.format("WAYPOINT:%d %nNombre: %s Tipo: %s %n\tDistancia del inicio: %,.2f Coordenadas: %dlongitud %dlatitud ",numeroWaypoint,nombreReto,tipo, distanciaInicio,longitud,latitud);
+    
+    }
